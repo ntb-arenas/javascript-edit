c1641c58658c43836bd9c20f82495aa8d3e74e65
Niño Arenas <ntb.arenas@protonmail.com>
2022-10-17 22:51:03 +0100

    first publish

---
+++ aula1/aula1.js
@@ -0,0 +1,23 @@
+let firstName = "Niño"; //string
+let lastName = "Arenas"; //string
+let age = 21; //number
+let isDeveloper = true; //boolean
+let country = "Portugal";
+let city = "Lisboa";
+
+// if (isDeveloper) {
+//   // Concatenation
+//   console.log("My name is " + firstName + " " + lastName + ", " + age + " years old. Lives in " + city + ", " + country + ". I'm a developer");
+
+//   // String literals
+//   console.log(`My name is ${firstName} ${lastName}, ${age} years old. Lives in ${city}, ${country}. I'm a developer`);
+// } else {
+//   console.log("My name is " + firstName + " " + lastName + ", " + age + " years old. Lives in " + city + ", " + country + ". I'm not a developer");
+//   console.log(`My name is ${firstName} ${lastName}, ${age} years old. Lives in ${city}, ${country}. I'm not a developer`);
+// }
+
+//Ex 3
+
+console.log(`O meu nome é ${firstName}`);
+console.log(`O ${firstName} tem ${age} anos`);
+console.log(`%cO ${firstName} vive em ${city}`, "color: red");
