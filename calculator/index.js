32641cc69dac89702155334d19020c0e0aacdfaf
Niño Arenas <ntb.arenas@protonmail.com>
2022-11-09 12:09:40 +0000

    update

--- calculator/index.js
+++ calculator/index.js
@@ -1,9 +1,9 @@
 let firstNum = document.querySelector(".first-num").value;
 let secondNum = document.querySelector(".second-num").value;
-let result = document.querySelector("#result");
-let calculate;
+let result = document.querySelector(".result");
 
-function add() {
-  calculate = firstNum + secondNum;
+function add(event, firstNum, secondNum) {
+  event.preventDefault();
+  let calculate = firstNum + secondNum;
   result.innerHTML = calculate;
 }
